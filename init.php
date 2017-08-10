9dc7b3a30f7a1a7f18aedffb7b8ae1182ebc24c0
Max Weller <mweller@d120.de>
2017-08-10 15:27:36 +0200

    indentation

--- init.php
+++ init.php
@@ -11,11 +11,11 @@ set_view_var("action", $_SERVER["PHP_SELF"]."?".$_SERVER["QUERY_STRING"]);
 set_view_var("title", "KDV");
 
 try {
-$db = new PDO(DATABASE_URI, DATABASE_USER, DATABASE_PASSWORD);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db = new PDO(DATABASE_URI, DATABASE_USER, DATABASE_PASSWORD);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(Exception $ex) {
-// avoid the default error message as it might display the db password
-die("<H2>Eile mit Weile</H2>Database connection failed!");
+  // avoid the default error message as it might display the db password
+  die("<H2>Eile mit Weile</H2>Database connection failed!");
 }
 
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
