016cada5cdaed665cfe5d6ce845ef4bd44480811
Max Weller <mweller@d120.de>
2018-02-15 00:21:13 +0100

    allow line breaks in wire transfer comments

--- views/core/ledger.php
+++ views/core/ledger.php
@@ -13,7 +13,7 @@ foreach($ledger as $d) {
   $storno_btn="";
   if ($i<3 && !$d["storno"]) $storno_btn = sprintf("<a href='#' onclick='return storno_payment(%d,%d)'>Stornieren</a>", $d["user_id"], $d["id"]);
   printf("<tr class='%s'><td>%s</td><td>%s</td><td class='col-comment'>%s</td><td>%04.2f</td><td>%s</td></tr>",
-         $cls, $d["timestamp"], $d["code"], ent($d["comment"]? $d["comment"] :$d["name"]), -($d["charge"]/100), $storno_btn);
+         $cls, $d["timestamp"], $d["code"], nl2br(ent($d["comment"]? $d["comment"] :$d["name"])), -($d["charge"]/100), $storno_btn);
   if (!$d["storno"]) $i++;
 }
 ?>
